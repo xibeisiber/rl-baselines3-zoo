5f797444c81d0445856b8cbe0827dbfb59beed75
jia <jiying.jia@peitian.com>
2023-04-28 15:54:40 +0800

    modify enjoy_cmd.py

--- enjoy_cmd.py
+++ enjoy_cmd.py
@@ -5,11 +5,14 @@ import glob
 
 envid = "BounceBall_Pybullet_env-v0"
 algo = "sac"
-exp_id = 1
+exp_id = 2
 
-logpath = os.path.join(os.path.dirname(__file__), "logs/%s/%s_%d/"%(algo, envid, exp_id))
+envconfig_path = os.path.join(os.path.dirname(__file__), "logs/%s/%s_%d/env_config.json"%(algo, envid, exp_id))
 
-with open(logpath+"env_config.json") as fr:
+if not os.path.exists(envconfig_path):
+    raise IOError("envconfig file does not exist. Path: %s")%envconfig_path
+
+with open(envconfig_path) as fr:
     envconfig = json.load(fr)
 
 print(">>> env_config:")
@@ -18,7 +21,7 @@ print(envconfig)
 envconfig_str="config:\"%s\""%envconfig
 
 # cmd = 'python enjoy.py --algo %s --env %s -f logs/ --exp-id %d --env-kwargs gym_render:True %s '%(algo, envid, exp_id, envconfig_str)
-cmd = 'python enjoy.py --algo %s --env %s -f logs/ --exp-id %d --load-best --best-n 0 --env-kwargs gym_render:True %s '%(algo, envid, exp_id, envconfig_str)
+cmd = 'python enjoy.py --algo %s --env %s -f logs/ --exp-id %d --load-best --env-kwargs gym_render:True %s '%(algo, envid, exp_id, envconfig_str)
 
 print(">>> enjoy command:")
 print(cmd)
