2a1e0d70fd749257e11fbdbc4383bdad02247141
yi.liang <yi.liangbj@peitian.com>
2023-06-01 09:24:46 +0800

    added hyperparams tuning option to train_cmd.py

--- train_cmd.py
+++ train_cmd.py
@@ -20,7 +20,7 @@ def get_latest_run_id(log_path: str, env_name: str) -> int:
     return max_run_id
 
 envid = "BounceBall_Pybullet_env-v0"
-algo="ppo"
+algo="sac"
 
 
 
@@ -45,27 +45,28 @@ f3=Figlet(font='standard',width=110)
 print(f3.renderText(str("Model ID "+runname) ))
 
 
+hyper_opt=True
 
 ### 训练时需要调整的参数
 pretrain = False  # 是否需要用预训练的模型
-pretrain_model_id = 32
+pretrain_model_id = 51
 pretrain_best_model_id = "best_model"   ###选择加载哪个bestmodel- 20230512脚本默认改为训练只有一个最佳model
 pretrain_best_model_flag = False     ###True则加载最佳模型，False加载最后模型
 if pretrain_best_model_flag:
     model_file = "%s.zip"%pretrain_best_model_id
 else:
     model_file = "%s.zip"%envid
-train_step=3e6
+train_step=5e6
 modelName_prefix=runname
 envconfig = {
     "00_modelname": "air4a", # "air4a", "air7l_b"
-    "000_initBallMethod": "toss", # "fall", "toss", "random"
+    "000_initBallMethod": "fall", # "fall", "toss", "random"
     "act_opt": 2,
-    "01_frame_skip": 80,
-    "02_robotobs_timelag": 80,
+    "01_frame_skip": 40,
+    "02_robotobs_timelag": 40,
     "03_ballobs_timelag": 114,
-    "04_act_timelag": 144,
-    "05_obs_stack_n": 20,
+    "04_act_timelag": 144,  
+    "05_obs_stack_n": 5,
     "07_rw_coeff_sparse": 1,
     "08_rw_coeff_paddlez": 1,
     "09_rw_coeff_ballvel": 3.5,
@@ -75,7 +76,7 @@ envconfig = {
     "rand_ballpos": 0.05,
     "rand_force_on_ball": 0.001,
     "rand_bounceforce_on_ball": 0.01,
-    "13_rand_obs_ballpos": 0.015,
+    "13_rand_obs_ballpos": 0.01,
     "14_rand_obs_ballvel": 0.1,
     "q_alpha": 0.01,
     "q_vel_alpha": 0.01,
@@ -91,14 +92,18 @@ envconfig = {
 pretrain_model_file = os.path.join(os.path.dirname(__file__), "logs/%s"%algo, "%s_%d"%(envid, pretrain_model_id), model_file)
 envconfig_str="config:\"%s\""%envconfig
 
-
-if pretrain:
-    print(">>> use pretrained model %s"%pretrain_model_file)
-    if not os.path.exists(pretrain_model_file):
-        raise IOError("Model %s does not exist"%pretrain_model_file)
-    cmd = 'python train.py --algo %s --env %s -i %s --save-model-n 1 --wandb-project-name %s --wandb-run-name %s --env-kwargs %s --n-timesteps %d --vec-env subproc --n-eval-envs 5 --eval-freq 50000 -P'%(algo, envid, pretrain_model_file, projname, runname, envconfig_str,train_step)
+ 
+if hyper_opt:
+    print(">>> Hyperparameters tuning")
+    cmd='python train.py --algo %s --env %s -n 50000 -optimize --wandb-project-name %s --wandb-run-name %s --env-kwargs %s --n-trials 100 --n-jobs 1 --sampler tpe --pruner median --n-evaluations 10000 --n-eval-envs 32  --verbose 0 --vec-env subproc -P'%(algo, envid, projname, runname, envconfig_str)
 else:
-    cmd = 'python train.py --algo %s --env %s --save-model-n 1 --wandb-project-name %s --wandb-run-name %s --env-kwargs %s --n-timesteps %d --vec-env subproc --n-eval-envs 5 --eval-freq 50000 -P'%(algo, envid, projname, runname, envconfig_str,train_step)
+    if pretrain:
+        print(">>> use pretrained model %s"%pretrain_model_file)
+        if not os.path.exists(pretrain_model_file):
+            raise IOError("Model %s does not exist"%pretrain_model_file)
+        cmd = 'python train.py --algo %s --env %s -i %s --save-model-n 1 --wandb-project-name %s --wandb-run-name %s --env-kwargs %s --n-timesteps %d --vec-env subproc --n-eval-envs 32 --eval-freq 70000 -P'%(algo, envid, pretrain_model_file, projname, runname, envconfig_str,train_step)
+    else:
+        cmd = 'python train.py --algo %s --env %s --save-model-n 1 --wandb-project-name %s --wandb-run-name %s --env-kwargs %s --n-timesteps %d --vec-env subproc --n-eval-envs 32 --eval-freq 70000 -P'%(algo, envid, projname, runname, envconfig_str,train_step)
 
 print(">>> Train command:")
 print(cmd)
